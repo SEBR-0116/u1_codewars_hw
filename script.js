a9c8960809ee1ce94be6bc6d9fa671b158e2df50
Denesh Anandathasan <denesh555@gmail.com>
2024-01-18 17:29:21 -0500

    Problem 1 : Resolved -Test

---
+++ script.js
@@ -0,0 +1,21 @@
+//1
+/*
+Kata.MakeNegative(1);  // return -1
+Kata.MakeNegative(-5); // return -5
+Kata.MakeNegative(0);  // return 0
+*/
+
+// const makeNegative = (num) => {
+//     if(!isNaN(num))
+//     {
+//         console.log(num = num * -1)
+//     }
+//     else{
+//         console.log ("NOT numbuer")
+//     }
+//      }
+
+const makeNegative = (num) => {
+    (!isNaN(num))? console.log(num = num * -1) : console.log ("NOT numbuer") }
+
+     makeNegative(10)
--- solution.md
+++ solution.md
@@ -2,6 +2,10 @@
 
 ```js
 
+const makeNegative = (num) => {
+    (!isNaN(num))? console.log(num = num * -1) : console.log ("NOT numbuer") }
+
+     makeNegative(10)
 ```
 
 ## Sum of Positive
