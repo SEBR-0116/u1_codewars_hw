6b0d9f78ab975d4f44bcbf63ebf7ed3485c498ba
whoever11 <cjthesedays000@gmail.com>
2024-01-18 21:13:58 -0500

    third one done

--- script.js
+++ script.js
@@ -5,8 +5,15 @@
 // //end of number 1
 
 // number 2 
- const numbers = [3, -2, 5, -7, 9, -1];
- const sumPositiveNumbers = numbers => numbers.reduce((sum, num) => num > 0 ? sum + num : sum, 0)
- console.log(sumPositiveNumbers(numbers))
+//  const numbers = [3, -2, 5, -7, 9, -1];
+//  const sumPositiveNumbers = numbers => numbers.reduce((sum, num) => num > 0 ? sum + num : sum, 0)
+//  console.log(sumPositiveNumbers(numbers))
 //end of number 2
 
+// number 3
+function squareNumber(number){
+    return number * number
+}
+let result = squareNumber(7)
+console.log(result)
+//end of number 3
